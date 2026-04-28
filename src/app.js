dc3e2bc68f6df08d4eba35db75e49699fa1ba4aa
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-04-28 17:51:26 +0300

    finish

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const telEl = document.querySelector(".tel");
 const emailEl = document.querySelector(".email");
 const btnEl = document.querySelector(".btn");
 const listEl = document.querySelector(".list");
-const arr = []
+const arr = JSON.parse(localStorage.getItem("contacts")) || [];
 
-btnEl.addEventListener("click", (event) => {
+btnEl.addEventListener("click", () => {
     const data = {
         name: nameEl.value,
         secondName: secondNameEl.value,
@@ -15,6 +15,7 @@ btnEl.addEventListener("click", (event) => {
     }
 
     arr.push(data);
+    localStorage.setItem("contacts", JSON.stringify(arr));
     console.log(data);
     addObj(arr);
 })
@@ -22,14 +23,22 @@ btnEl.addEventListener("click", (event) => {
 function addObj(arr) {
     const item = arr.map((item, index) => {
         return `<li class="contact">
-                    <p class="text">Контакт ${nameEl.value}</p>
-                    <p class="text">Ім'я: ${nameEl.value}</p>
-                    <p class="text">Прізвище: ${secondNameEl.value}</p>
-                    <p class="text">Телефон: ${telEl.value}</p>
-                    <p class="text">Електронна пошта: ${emailEl.value}</p>
+                    <p class="text">Контакт ${item.name}</p>
+                    <p class="text">Ім'я: ${item.name}</p>
+                    <p class="text">Прізвище: ${item.secondName}</p>
+                    <p class="text">Телефон: ${item.tel}</p>
+                    <p class="text">Електронна пошта: ${item.email}</p>
                     <button type="button" data-index="${index}">Видалити</button>
-                    <button type="button" data-index="${index}">Редагуати</button>
                 </li>`;
     }).join("");
     listEl.innerHTML = item;
 }
+
+listEl.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        const index = event.target.dataset.index;
+        arr.splice(index, 1);
+        localStorage.setItem("contacts", JSON.stringify(arr));
+        addObj(arr);
+    }
+});
